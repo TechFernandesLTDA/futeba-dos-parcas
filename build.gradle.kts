3d9beb2026ce1bfca46efec78d18c2fb13e3eb97
Renan Locatiz Fernandes <renan.fernandes@outlook.com>
2026-01-19 01:04:46 -0300

    aaa

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.13.2" apply false
-    id("com.android.library") version "8.13.2" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.android.application") version "9.0.0" apply false
+    id("com.android.library") version "9.0.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.10" apply false
     id("org.jetbrains.kotlin.multiplatform") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "2.0.21" apply false
-    id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
+    id("com.google.devtools.ksp") version "2.3.2" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.8.5" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.9.6" apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
     id("com.google.firebase.crashlytics") version "3.0.2" apply false
-    id("com.google.firebase.firebase-perf") version "1.4.2" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.google.firebase.firebase-perf") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
 }
